329c2380274796b44d0121ef24069a917815eb34
유희주 <rubyruby2000@naver.com>
2020-10-31 23:04:15 +0900

    initial

---
+++ star.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int input, i,j;
+	scanf("%d", &input);
+   for(i=1; i <= input; i++){
+	   for(j=1; j<=i; j++)
+		   printf("*");
+	   printf("\n");
+   }
+   return 0;
+}
