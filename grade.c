e25d53ebd476e07133e2bb6977270fa1b44c3374
유희주 <rubyruby2000@naver.com>
2020-10-31 21:46:11 +0900

    initial

---
+++ grade.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int score;
+	scanf("%d", &score);
+
+	if(scare >=90)
+		printf("A\n");
+
+	else if(scare >=80)
+      printf("B\n");
+
+    else if(scare >=70)
+      printf("C\n");
+ 
+	else if (sore >=60)
+		printf("D\n");
+
+	else if(scare >=50)
+         printf("F\n");
+
+	return 0;
+}
