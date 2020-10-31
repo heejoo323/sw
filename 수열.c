37f718d022f9d038da2d7d47df1bc020fe3dbfb2
유희주 <rubyruby2000@naver.com>
2020-11-01 00:31:07 +0900

    initial

---
+++ 수열.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int N, X, input;
+
+	scanf("%d %d", &N, &X);
+
+	for (int i=0; i<NL i++){
+		scanf("%d", &input);
+
+		if(X>input)
+			printf("%d", input);
+	}
+	return 0;
+}
